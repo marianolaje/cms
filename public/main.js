961dc96fc0a5e952cc27ad223f563ad4bcea640d
Mariano Laje <marianolaje@outlook.com>
2020-08-13 19:19:08 +0200

    added FORM and Zendesk to CMS

--- public/main.js
+++ public/main.js
@@ -4,9 +4,13 @@ const fs = require("fs")
 const dirTitlePath = path.join(__dirname, "../src/info/title")
 const dirSubtitlePath = path.join(__dirname, "../src/info/subtitle")
 const dirInformationPath = path.join(__dirname, "../src/info/information")
+const dirFormList = path.join(__dirname, "../src/info/form")
+const dirTagZendesk = path.join(__dirname, "../src/info/tagZendesk")
 let titlelist = []
 let subtitlelist = []
 let informationList = []
+let formList = []
+let tagZendeskList = []
 
 const getTitles = () => {
     fs.readdir(dirTitlePath, (err, files) => {
@@ -182,8 +186,112 @@ const getInformation = () => {
     return
 }
 
+const getForms = () => {
+    fs.readdir(dirFormList, (err, files) => {
+        if (err) {
+            return console.log("Failed to list contents of directory: " + err)
+        }
+        let ilist = []
+        files.forEach((file, i) => {
+            let obj = {}
+            let post
+            fs.readFile(`${dirFormList}/${file}`, "utf8", (err, contents) => {
+                const getMetadataIndices = (acc, elem, i) => {
+                    if (/^---/.test(elem)) {
+                        acc.push(i)
+                    }
+                    return acc
+                }
+                const parseMetadata = ({lines, metadataIndices}) => {
+                    if (metadataIndices.length > 0) {
+                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
+                        metadata.forEach(line => {
+                            line = line.replace('\r', '')
+                            obj[line.split(": ")[0]] = line.split(": ")[1]
+                        })
+                        return obj
+                    }
+                }
+                const lines = contents.split("\n")
+                const metadataIndices = lines.reduce(getMetadataIndices, [])
+                const metadata = parseMetadata({lines, metadataIndices})
+                post = {
+                    id: metadata.id,
+                    label: metadata.title,
+                    helperText: metadata.helperText ? metadata.helperText : null,
+                }
+                informationList.push(post)
+                ilist.push(i)
+                if (ilist.length === files.length) {
+                    const sortedList = formList.sort ((a, b) => {
+                        return a.id < b.id ? 1 : -1
+                    })
+                    let data = JSON.stringify(sortedList)
+                    fs.writeFileSync("src/formJson/form.json", data)
+                }
+            })
+        })
+    })
+    return
+}
+
+const getTagZendesk = () => {
+    fs.readdir(dirTagZendesk, (err, files) => {
+        if (err) {
+            return console.log("Failed to list contents of directory: " + err)
+        }
+        let ilist = []
+        files.forEach((file, i) => {
+            let obj = {}
+            let post
+            fs.readFile(`${dirTagZendesk}/${file}`, "utf8", (err, contents) => {
+                const getMetadataIndices = (acc, elem, i) => {
+                    if (/^---/.test(elem)) {
+                        acc.push(i)
+                    }
+                    return acc
+                }
+                const parseMetadata = ({lines, metadataIndices}) => {
+                    if (metadataIndices.length > 0) {
+                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
+                        metadata.forEach(line => {
+                            line = line.replace('\r', '')
+                            obj[line.split(": ")[0]] = line.split(": ")[1]
+                        })
+                        return obj
+                    }
+                }
+                const lines = contents.split("\n")
+                const metadataIndices = lines.reduce(getMetadataIndices, [])
+                const metadata = parseMetadata({lines, metadataIndices})
+                const date = new Date()
+                const timestamp = date.getTime() / 1000
+                post = {
+                    id: timestamp,
+                    title: metadata.title,
+                    tagOne: metadata.tagOne,
+                    tagTwo: metadata.tagTwo ? metadata.tagTwo : null,
+                    tagThree: metadata.tagThree ? metadata.tagThree : null,
+                }
+                informationList.push(post)
+                ilist.push(i)
+                if (ilist.length === files.length) {
+                    const sortedList = tagZendeskList.sort ((a, b) => {
+                        return a.id < b.id ? 1 : -1
+                    })
+                    let data = JSON.stringify(sortedList)
+                    fs.writeFileSync("src/formJson/tagZendesk.json", data)
+                }
+            })
+        })
+    })
+    return
+}
+
 console.log("hola")
 
 getTitles()
 getSubtitles()
 getInformation()
+getForms()
+getTagZendesk()
